aef4548e1df92ef022aa713d160a0c5a41dde26a
40327382 <ggoodwin04@qub.ac.uk>
2024-03-23 16:55:57 +0000

    updated relapse after 60 months logic

--- Website/app.py
+++ Website/app.py
@@ -52,18 +52,18 @@ def unauthenticated():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@nocache
 def login():
     if logged_in():
         return redirect(url_for('form'))
-    error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+            flash('Invalid Credentials. Please try again.')
         else:
             session['logged_in'] = True
             return redirect(url_for('form'))
 
-    return render_template('login.html', error=error)
+    return render_template('login.html')
 
 
 @app.route('/logout')
@@ -161,6 +161,10 @@ def about():
         flash('Please login to continue.')
         return redirect(url_for('unauthenticated'))
     return render_template("about.html")
+
+@app.route('/savedPredictions')
+def savedPredictions():
+    return render_template('savedPredictions.html')
 
 
 if __name__ == '__main__':
